019c276f517e3c93edb763ad366f63e45178a958
benpm <schwalegos@gmail.com>
2021-05-21 13:55:29 -0600

    improvements

--- code/strats/metaDetective.py
+++ code/strats/metaDetective.py
@@ -1,6 +1,9 @@
 # Meta-detective: the final form
 
 import numpy as np
+from enum import Enum
+
+rnd = np.random.random
 
 def s_def(H: np.ndarray, M): # Always defect
     return 0, None
@@ -12,7 +15,9 @@ def s_alt(H: np.ndarray, M): # Alternate
     return 1-H[0,-1], None
 def s_al4(H: np.ndarray, M): # Co-op, defect x 3, repeat
     return [1,0,0,0][H.shape[1] % 4], None
-def s_ant(H: np.ndarray, M): # Anti-joss
+def s_la4(H: np.ndarray, M): # Co-op x3, defect, repeat
+    return [1,1,1,0][H.shape[1] % 4], None
+def s_ant(H: np.ndarray, M): # Anti-joss with tiny bit of defection
     if M is None:
         M = 1
     elif H[1,-1] == 0:
@@ -22,25 +27,26 @@ def s_ant(H: np.ndarray, M): # Anti-joss
     else:
         return s_alt(H,M)
 def s_jos(H: np.ndarray, M): # Joss with high probability
-    return 0 if np.random.random() < 0.15 else s_t4t(H,M)[0], None
+    return 0 if (rnd() < 0.15 and H[0,-1]==1) else s_t4t(H,M)[0], None
+def s_jos(H: np.ndarray, M): # Joss with low probability
+    return 0 if (rnd() < 0.0 and H[0,-1]==1) else s_t4t(H,M)[0], None
 
 DEFAULT_STRAT = s_def
-X_T4T = s_ant # Strategy to use for tit-for-tat-likes
 
 TEST_PATTERN = (1,0,0,1,1)
-PATTERN_FILE = open("md.txt", "w")
-TEST_MODE = True
+TEST_MODE = False
+if TEST_MODE:
+    PATTERN_FILE = open("md.txt", "w")
 PATTERNS = {
-    (1,1,0,1,1): s_jos, # simpleton
-    (1,1,1,0,1): s_alt, # ftft
-    # ?                 # random
-    (1,1,0,0,0): s_def, # grimTrigger
-    (1,0,1,1,1): s_cop, # detective
-    (1,1,0,0,1): X_T4T, # joss
-    (1,1,0,0,1): X_T4T, # titForTat
-    (1,1,0,1,1): s_jos, # oracle
-    (0,0,0,0,0): s_def, # alwaysDefect
-    (1,1,1,1,1): s_def, # alwaysCooperate
+    (1,1,1,0,1): s_alt, # [ftft]
+    (1,1,0,0,0): s_def, # [grimTrigger]
+    (1,0,1,1,1): s_cop, # some detectives
+    (1,1,0,0,1): s_ant, # [joss], [titForTat]
+    (1,1,0,1,1): s_jos, # [oracle], [simpleton]
+    (0,0,0,0,0): s_def, # [alwaysDefect]
+    (1,1,1,1,1): s_def, # [alwaysCooperate]
+    (1,0,0,1,1): s_la4, # most other detectives
+    (1,1,1,1,0): s_la4, # [shortwindow]
 }
 
 def strategy(history: np.ndarray, memory):
